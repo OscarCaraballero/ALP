f128c2901b432b594cbd4f0016ef054aae6718a4
Jesua Betancor Alemán <jbetancor.0011@gmail.com>
2014-12-10 15:07:21 +0000

    Creada ventana Contacto y Servicios

---
+++ view/ContactoView.php
@@ -0,0 +1,11 @@
+<div class="grid-100 disVid">
+    <div class="grid-50 contacto">
+        <h1>Contacto</h1>
+        <ul>
+            <li>Cosa 1</li>
+            <li>Cosa 2</li>
+            <li>Cosa 3</li>
+            <li>Cosa 4</li>
+        </ul>
+    </div>
+</div>
---
+++ view/ServiciosView.php
@@ -0,0 +1,34 @@
+<div class="grid-100 disVid">
+    <div class="grid-30 servicios">
+        <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button">
+            <span class="ui-button-text">Piscina</span>
+        </button>
+                
+        <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button">
+            <span class="ui-button-text">WiFi</span>
+        </button>
+        
+        <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button">
+            <span class="ui-button-text">Animales</span>
+        </button>
+        
+        <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button">
+            <span class="ui-button-text">Servicio de habitaciones</span>
+        </button>
+        
+    </div>
+    
+    <div class="grid-70 serviciosDesc">
+        <h1>Título</h1>
+        
+        <div class="grid-65" descripcion>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus felis. In hac habitasse platea dictumst. Quisque laoreet posuere lectus at porttitor. Donec vitae quam lacinia velit suscipit pulvinar. Aliquam sollicitudin libero nulla, quis tincidunt urna faucibus quis. Integer nec ipsum ut ex consectetur laoreet. Fusce a felis lacinia, placerat neque a, rhoncus lacus. Nulla egestas fermentum feugiat. Integer ut lacus vitae mauris interdum lacinia.
+
+Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin scelerisque volutpat posuere. Etiam lectus neque, finibus vestibulum vehicula at, porttitor et arcu. Vestibulum tincidunt blandit aliquet. Sed in dictum magna, eget cursus metus. Phasellus id laoreet quam. Etiam id tellus et risus dapibus auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris at metus elit. Donec at dignissim dui. Etiam cursus ipsum ac sapien lobortis, quis porta nisl feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus convallis et ipsum eu gravida. Donec leo justo, fermentum sed erat in, imperdiet aliquam nisl. </p>
+
+        </div>
+        <div class="grid-35" fotoDesc>
+            <p>FOTO</p>
+        </div>
+    </div>
+</div>
