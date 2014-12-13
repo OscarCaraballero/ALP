fb4dcb0edfc8394658824d9780cc8706d5c9ef73
ocaraballero <ocaraballero@ocaraballero-PC>
2014-12-13 14:06:09 +0000

    Formulario de disponibilidad con style

--- view/InicioView.php
+++ view/InicioView.php
@@ -2,49 +2,42 @@
     <div class="grid-50 disponibilidad">
         <form class="form" action="" method="post" >
             <fieldset>
-                <div>
-                    <label>Entrada</label>
-                    <input type="text" id="entrada">
-                </div>
-                <div>
-                    <label>Salida</label>
-                    <input type="text" id="salida">
-                </div>
-                <div>
-                    <label>Adultos</label>
-                    <select name="adultos" id="number">
-                        <option>1</option>
-                        <option selected="selected">2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                    </select>
-                </div>    
-                <div>
-                    <label>Niños</label>
-                    <select name="niños" id="niños">
-                        <option selected="selected">1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-                <div>
-                    <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button" type="submit">
-                        <span class="ui-button-text">Disponibilidad</span>
-                    </button>
-                </div>
-                <fieldset>
+                <label>Entrada</label>
+                <input type="text" id="entrada">
+                <label>Adultos</label>
+                <select name="adultos" id="number">
+                    <option>1</option>
+                    <option selected="selected">2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                </select>
+            </fieldset>
+            <fieldset>
+                <label>Salida</label>
+                <input type="text" id="salida">
+                
+                <label>Niños</label>
+                <select name="niños" id="niños">
+                    <option selected="selected">1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>          
+                <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button" type="submit">
+                    <span class="ui-button-text">Disponibilidad</span>
+                </button>
+            </fieldset>
         </form>           
     </div>
     <div class="grid-50 video">
-        <iframe width="588" height="250" src="//www.youtube-nocookie.com/embed/kPDnw3_1GOI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        <iframe width="580" height="250" src="//www.youtube-nocookie.com/embed/kPDnw3_1GOI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>
 <div class="grid-100 lorem">
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus felis. In hac habitasse platea dictumst. Quisque laoreet posuere lectus at porttitor. Donec vitae quam lacinia velit suscipit pulvinar. Aliquam sollicitudin libero nulla, quis tincidunt urna faucibus quis. Integer nec ipsum ut ex consectetur laoreet. Fusce a felis lacinia, placerat neque a, rhoncus lacus. Nulla egestas fermentum feugiat. Integer ut lacus vitae mauris interdum lacinia.
 
-Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin scelerisque volutpat posuere. Etiam lectus neque, finibus vestibulum vehicula at, porttitor et arcu. Vestibulum tincidunt blandit aliquet. Sed in dictum magna, eget cursus metus. Phasellus id laoreet quam. Etiam id tellus et risus dapibus auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris at metus elit. Donec at dignissim dui. Etiam cursus ipsum ac sapien lobortis, quis porta nisl feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus convallis et ipsum eu gravida. Donec leo justo, fermentum sed erat in, imperdiet aliquam nisl. </p>
+        Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin scelerisque volutpat posuere. Etiam lectus neque, finibus vestibulum vehicula at, porttitor et arcu. Vestibulum tincidunt blandit aliquet. Sed in dictum magna, eget cursus metus. Phasellus id laoreet quam. Etiam id tellus et risus dapibus auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris at metus elit. Donec at dignissim dui. Etiam cursus ipsum ac sapien lobortis, quis porta nisl feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus convallis et ipsum eu gravida. Donec leo justo, fermentum sed erat in, imperdiet aliquam nisl. </p>
 </div>
