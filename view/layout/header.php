f6e1a4c84e3cf3c9e8a8a69068f3bfe34007eb2a
ocaraballero <ocaraballero@ocaraballero-PC>
2014-12-14 19:39:18 +0000

    modal

--- view/layout/header.php
+++ view/layout/header.php
@@ -115,6 +115,24 @@
            });
         });
         </script>
+         <script>
+            $(function() {
+            $( "#dialog" ).dialog({
+            autoOpen: false,
+            show: {
+            effect: "blind",
+            duration: 1000
+            },
+            hide: {
+            effect: "blind",
+            duration: 1000
+            }
+            });
+            $( "#opener" ).click(function() {
+            $( "#dialog" ).dialog( "open" );
+            });
+            });
+        </script>
     </head>
     <body>
         <div id="wrapper" class="grid-container">
@@ -165,10 +183,11 @@
                     </form>
                 </div>
                 <div class="grid-20" id="bheader">
-                    <button role="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" id="button">
-                        <span class="ui-button-text">Login</span>
-                    </button>
+                    <a id="opener" href="#" id="dialog-link" class="ui-state-default ui-corner-all">Login</a>
                 </div>
-        	</div>
-		</div>
+            </div>
+        </div>
             <div id="content" class="grid-100">
+                <div id="dialog" title="Basic dialog">
+                    <p>This is an animated dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+                </div>
