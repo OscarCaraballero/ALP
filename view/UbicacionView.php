0ca6d7d5aed1bc9eeef569c45410b1162f867690
Jesua Betancor Alemán <jbetancor.0011@gmail.com>
2014-12-14 16:12:53 +0000

    Mapa insertado en ubicación

--- view/UbicacionView.php
+++ view/UbicacionView.php
@@ -1,6 +1,9 @@
 <div class="grid-100 disVid">
     <div class="grid-65 ubicacion">
-            <p> Apartamentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus felis. In hac habitasse platea dictumst. Quisque laoreet posuere lectus at porttitor. Donec vitae quam lacinia velit suscipit pulvinar. Aliquam sollicitudin libero nulla, quis tincidunt urna faucibus quis. Integer nec ipsum ut ex consectetur laoreet. Fusce a felis lacinia, placerat neque a, rhoncus lacus. Nulla egestas fermentum feugiat. Integer ut lacus vitae mauris interdum lacinia.
+            <p> mapa: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus felis. In hac habitasse platea dictumst. Quisque laoreet posuere lectus at porttitor. Donec vitae quam lacinia velit suscipit pulvinar. Aliquam sollicitudin libero nulla, quis tincidunt urna faucibus quis. Integer nec ipsum ut ex consectetur laoreet. Fusce a felis lacinia, placerat neque a, rhoncus lacus. Nulla egestas fermentum feugiat. Integer ut lacus vitae mauris interdum lacinia.
+Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin scelerisque volutpat posuere. Etiam lectus neque, finibus vestibulum vehicula at, porttitor et arcu. Vestibulum tincidunt blandit aliquet. Sed in dictum magna, eget cursus metus. Phasellus id laoreet quam. Etiam id tellus et risus dapibus auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris at metus elit. Donec at dignissim dui. Etiam cursus ipsum ac sapien lobortis, quis porta nisl feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus convallis et ipsum eu gravida. Donec leo justo, fermentum sed erat in, imperdiet aliquam nisl. </p>
+            <br>
+            <p> mapa: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus felis. In hac habitasse platea dictumst. Quisque laoreet posuere lectus at porttitor. Donec vitae quam lacinia velit suscipit pulvinar. Aliquam sollicitudin libero nulla, quis tincidunt urna faucibus quis. Integer nec ipsum ut ex consectetur laoreet. Fusce a felis lacinia, placerat neque a, rhoncus lacus. Nulla egestas fermentum feugiat. Integer ut lacus vitae mauris interdum lacinia.
 Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin scelerisque volutpat posuere. Etiam lectus neque, finibus vestibulum vehicula at, porttitor et arcu. Vestibulum tincidunt blandit aliquet. Sed in dictum magna, eget cursus metus. Phasellus id laoreet quam. Etiam id tellus et risus dapibus auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris at metus elit. Donec at dignissim dui. Etiam cursus ipsum ac sapien lobortis, quis porta nisl feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus convallis et ipsum eu gravida. Donec leo justo, fermentum sed erat in, imperdiet aliquam nisl. </p>
     </div>
     
@@ -10,9 +13,11 @@ Aliquam ac tincidunt nulla. Phasellus pulvinar elementum nisl at accumsan. Proin
             <img src="view/img/apartamentos.jpg" alt="" />
         </a>
         
-        <a class="fotoDesc" href="view/img/apartamentos.jpg">
-            <img src="view/img/apartamentos.jpg" alt="" />
-        </a>
+        <iframe class="mapa"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3519.5548921197365!2d-15.419847!3d28.099117!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc409514173e77eb%3A0xbda0edfa5e221aaa!2sUniversidad+de+Las+Palmas+de+Gran+Canaria+-+ULPGC!5e0!3m2!1ses!2ses!4v1418573076287" 
+            width="600" height="450" frameborder="0" style="border:0">
+                
+        </iframe>
 
     </div>
 </div>
